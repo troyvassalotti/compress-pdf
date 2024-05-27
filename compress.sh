5f41a4052b2462d6c6823f6210c97bc9e73f2a2a
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2024-05-26 21:27:13 -0400

    Add script

---
+++ compress.sh
@@ -0,0 +1,3 @@
+pdfcompress () {
+   gs -q -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=144 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=144 -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=144 -sOutputFile=${1%.*}.compressed.pdf $1; 
+}
